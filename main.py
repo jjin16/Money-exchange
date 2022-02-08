924a976e93d16be3ab1f5c2f6ee632db9c13b417
Jiarong Jin <98533787+jjin16@users.noreply.github.com>
2022-02-08 13:55:55 +0000

    added f string onto the last print statement on line 9

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ USD = money_input * rate
 exchange_fee= 3
 amount_for_user= USD- exchange_fee
 amount_to_exchange = round(amount_for_user)
-print("Amount to exchange: "+ str(amount_to_exchange))
+##print("Amount to exchange: "+ str(amount_to_exchange))
+print(f"amount to exchange: {amount_to_exchange}")
