86b6443af5bfcb9b45ffdb83bfabcd74aa49e2c6
JiarongJin <98533787+jjin16@users.noreply.github.com>
2022-02-08 13:45:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+rate=float(input("What is the current exchange rate for the Euro to Dollar? (EUR to USD):")
+)
+money_input= float(input("How much Euro for exchange?:"))
+USD = money_input * rate
+exchange_fee= 3
+amount_for_user= USD- exchange_fee
+amount_to_exchange = round(amount_for_user)
+print("Amount to exchange: "+ str(amount_to_exchange))
